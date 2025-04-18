07ed31ca8c0455eabd3ae28c61e6ad38d64b5b78
Thet Aung <thetaung.dev@gmail.com>
2025-04-18 09:08:18 +0300

    chore: deleted unnecessary files and added example bug

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import '../../unistyles'
+
+import { Slot } from 'expo-router';
+import 'react-native-reanimated';
+
+
+export default function RootLayout() {
+  return <Slot />
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,36 @@
+import { Text } from "react-native"
+import { StyleSheet } from 'react-native-unistyles'
+
+const styles = StyleSheet.create(theme => ({
+    text: {
+        _web: {
+            fontSize: 50         
+        },
+
+        variants: {
+            variant: {
+                primary: {
+                    color: theme.colors.primary,
+
+                   
+                },
+                secondary: {
+                    color: theme.colors.secondary,
+
+                }
+            }
+        }
+    }
+}))
+
+const Home = () => {
+    return (
+        <>
+            <Text style={[styles.text, { fontSize: 12 }]}>
+                My fontSize should be 12
+            </Text>
+        </>
+    )
+}
+
+export default Home
